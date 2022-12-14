77123d945db2976b0a359e6de583013948e2c31a
mrevanzak <mrevanzak@gmail.com>
2022-12-14 17:05:05 +0700

    refactor: remove unused file

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,21 +1,13 @@
 import { AnyAction, combineReducers } from 'redux';
 
-import CartReducer from '@/redux/reducers/Cart';
-import CheckoutReducer from '@/redux/reducers/Checkout';
-import HistoryReducer from '@/redux/reducers/History';
 import OrderReducer from '@/redux/reducers/Orders';
 import ProductReducer from '@/redux/reducers/Products';
 import ReportReducer from '@/redux/reducers/Report';
-import ReviewReducer from '@/redux/reducers/Review';
 import UserReducer from '@/redux/reducers/User';
 
 const appReducer = combineReducers({
   products: ProductReducer,
-  cart: CartReducer,
   user: UserReducer,
-  history: HistoryReducer,
-  checkout: CheckoutReducer,
-  review: ReviewReducer,
   orders: OrderReducer,
   report: ReportReducer,
 });
