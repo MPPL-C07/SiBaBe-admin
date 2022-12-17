f8bf54a0f5c8dfe431c2a6f8a662821b214cb7a5
mrevanzak <mrevanzak@gmail.com>
2022-12-18 01:39:19 +0700

    feat(AddProductionModal): multiple data production entries refactor(AddProductionModal): code improvement

--- src/components/AddProduction.tsx
+++ src/components/AddProduction.tsx
@@ -2,6 +2,7 @@ import { Group, Image, Text } from '@mantine/core';
 import { Dropzone, FileWithPath, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import axios from 'axios';
 import * as React from 'react';
+import { BsPlusLg } from 'react-icons/bs';
 import { FiImage, FiUpload, FiXCircle } from 'react-icons/fi';
 import { RiCloseFill } from 'react-icons/ri';
 import { toast } from 'react-toastify';
@@ -15,24 +16,14 @@ import { API_KEY } from '@/pages/api/products';
 import { addProduction } from '@/redux/actions/Report';
 
 type AddProductionProps = {
-  // product: Product;
   setOpened: (opened: boolean) => void;
 };
 
-export default function AddProduction({
-  // product,
-  setOpened,
-}: AddProductionProps) {
+export default function AddProduction({ setOpened }: AddProductionProps) {
   //useRef
-  const nameRef = React.useRef<HTMLInputElement>(null);
-  const totalPriceRef = React.useRef<HTMLInputElement>(null);
+  const [name, setName] = React.useState(['']);
+  const [totalPrice, setTotalPrice] = React.useState([0]);
   const dateRef = React.useRef<HTMLInputElement>(null);
-
-  //usestate
-  // const [name, setName] = React.useState(product.name);
-  // const [price, setPrice] = React.useState(product.price);
-  // const [description, setDescription] = React.useState(product.description);
-  // const [stock, setStock] = React.useState(product.stock);
 
   const [files, setFiles] = React.useState<FileWithPath[]>([]);
   const [loading, setLoading] = React.useState(false);
@@ -46,19 +37,16 @@ export default function AddProduction({
       return;
     }
 
-    if (nameRef.current && totalPriceRef.current && dateRef.current) {
-      const name = nameRef.current.value;
-      const price = totalPriceRef.current.value;
+    if (dateRef.current) {
       const date = dateRef.current.value;
       const image = await onUpload();
 
-      dispatch(addProduction(date, name, Number(price), image));
-
-      // clear
-      nameRef.current.value = '';
-      totalPriceRef.current.value = '';
-      dateRef.current.value = '';
-      setFiles([]);
+      for (let i = 0; i < name.length; i++) {
+        dispatch(
+          addProduction(date, name[i], totalPrice[i], image)
+        );
+      }
+      setOpened(false);
     }
   };
 
@@ -90,10 +78,6 @@ export default function AddProduction({
       .then((res) => {
         setLoading(false);
         return res.data.data.url as string;
-        // if (invoice) {
-        // dispatch(confirmPayment(invoice, res.data.data.medium.url));
-        // setOpened(false);
-        // }
       });
     // .catch((err) => {
     //   setLoading(false);
@@ -109,13 +93,6 @@ export default function AddProduction({
             className='absolute top-7 right-7 z-10 cursor-pointer text-4xl'
             onClick={() => setOpened(false)}
           />
-          {/* <NextImage
-          useSkeleton
-          src={product.image}
-          alt={product.name}
-          width={1024}
-          height={320}
-        /> */}
           <div>
             <Dropzone
               onDrop={setFiles}
@@ -158,43 +135,71 @@ export default function AddProduction({
             </Dropzone>
             <Separator height={1.5} width='50%' className='mx-auto' />
           </div>
-          <div className='ml-14 mt-7'>
-            <p className='text-center font-secondary text-2xl'>
+          <div className='mx-14 mt-7 space-y-4'>
+            <h3 className='text-center font-secondary'>
               Data Produksi Tambahan
-            </p>
+            </h3>
             <form className='flex flex-col' id='form' onSubmit={handleSubmit}>
-              <label>Tanggal</label>
+              <label className='ml-5'>Tanggal</label>
               <input
-                className='my-5 w-max  max-w-4xl rounded-[50px]'
+                className='w-full rounded-full border-none'
                 type='date'
                 required
                 ref={dateRef}
               />
-              <Separator width='30%' height={2} className='mx-auto' />
-              <div className='flex flex-row justify-between py-5 pr-10'>
-                <div className='flex flex-col'>
-                  <label>Nama</label>
-                  <input
-                    className='my-5 w-96 max-w-4xl rounded-[50px]'
-                    type='text'
-                    placeholder='Fill the name of additional Product'
-                    required
-                    ref={nameRef}
-                  />
+              <Separator width='30%' height={1} className='mx-auto my-5' />
+
+              {name?.map((_, index) => (
+                <div className='flex flex-row gap-12 my-2' key={index}>
+                  <div className='flex flex-1 flex-col'>
+                    <label className='ml-5'>Nama</label>
+                    <input
+                      className='rounded-full border-none'
+                      type='text'
+                      placeholder='Fill the name of additional Product'
+                      required
+                      value={name[index]}
+                      onChange={(e) => {
+                        setName((prev) => {
+                          const arr = [...prev];
+                          arr[index] = e.target.value;
+                          return arr;
+                        });
+                      }}
+                    />
+                  </div>
+                  <div className='flex flex-1 flex-col'>
+                    <label className='ml-5'>Harga</label>
+                    <input
+                      className='rounded-full border-none'
+                      type='text'
+                      placeholder='Fill the price of it'
+                      required
+                      value={totalPrice[index]}
+                      onChange={(e) => {
+                        setTotalPrice((prev) => {
+                          const arr = [...prev];
+                          arr[index] = Number(e.target.value);
+                          return arr;
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
-                <div className='flex flex-col'>
-                  <label>Harga</label>
-                  <input
-                    className='my-5 w-96 max-w-4xl rounded-[50px]'
-                    type='text'
-                    placeholder='Fill the price of it'
-                    required
-                    ref={totalPriceRef}
-                  />
-                </div>
+              ))}
+              <div
+                className='animated-underline custom-link my-3 mr-2 flex cursor-pointer flex-row items-center gap-2 self-end border-b border-dotted border-dark hover:border-black/0'
+                onClick={() => {
+                  setName([...name, '']);
+                  setTotalPrice([...totalPrice, 0]);
+                }}
+              >
+                <p className='text-sm'>Tambah Data </p>
+                <BsPlusLg />
               </div>
+
               <Button
-                className='self-center rounded-full bg-brown py-4 px-24 font-secondary font-bold'
+                className='self-center rounded-full border-none bg-brown py-4 px-24 font-secondary font-bold'
                 type='submit'
                 form='form'
               >
